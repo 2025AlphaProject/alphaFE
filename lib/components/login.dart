af519b8ae74522b17d56d3299cb392167df916b2
47813 <vanguard0956@gmail.com>
2025-05-01 19:48:07 +0900

    [MODIFY]: login.dart 삭제 및 로그인 함수 전역 선언

---
+++ lib/components/login.dart
@@ -0,0 +1,59 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:alpha_fe/mainscreen.dart';
+
+class KakaoLoginService {
+  static Future<void> login(BuildContext context, String kakaoNativeAppKey) async {
+    KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
+    try {
+      OAuthToken token;
+
+      if (await isKakaoTalkInstalled()) {
+        try {
+          token = await UserApi.instance.loginWithKakaoTalk();
+          print('✅ 카카오톡 로그인 성공');
+        } catch (error) {
+          print('⚠️ 카카오톡 로그인 실패: $error');
+          print('👉 웹 로그인으로 대체합니다');
+          token = await UserApi.instance.loginWithKakaoAccount();
+          print('✅ 웹 로그인 성공');
+        }
+      } else {
+        token = await UserApi.instance.loginWithKakaoAccount();
+        print('✅ 웹 로그인 성공');
+      }
+
+      print('✅ ID Token: ${token.idToken}');
+
+      User user = await UserApi.instance.me();
+      List<String> scopes = [];
+
+      if (user.kakaoAccount?.profileNeedsAgreement == true) {
+        scopes.add('profile_nickname');
+      }
+
+      if (scopes.isNotEmpty) {
+        token = await UserApi.instance.loginWithNewScopes(scopes);
+        user = await UserApi.instance.me();
+      }
+
+      final dio = Dio();
+      final formData = FormData.fromMap({'id_token': token.idToken});
+      await dio.post(
+        'http://conever.duckdns.org:8000/auth/login/',
+        data: formData,
+        options: Options(headers: {'Accept': 'application/json'}),
+      );
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(accessToken: token.accessToken),
+        ),
+      );
+    } catch (e) {
+      print('❌ 로그인 전체 실패: $e');
+    }
+  }
+}
