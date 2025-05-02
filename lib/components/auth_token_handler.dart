b7c27237dab3888bfdb894bfc1231f69a5c273ea
47813 <vanguard0956@gmail.com>
2025-05-02 20:49:49 +0900

    [MODIFY]: 카카오 로그인 접근 방식 변경

---
+++ lib/components/auth_token_handler.dart
@@ -0,0 +1,25 @@
+import 'package:alpha_fe/components/refresh_token_controller.dart';
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
+
+Future<String?> getAccessTokenFromRefreshToken() async {
+  final refreshToken = getRefreshToken();
+
+  if (refreshToken == null) return null;
+
+  try {
+    final dio = Dio();
+    final formData = FormData.fromMap({'refresh_token': refreshToken});
+    final response = await dio.post(
+      'http://conever.duckdns.org:8000/auth/refresh/',
+      data: formData,
+      options: Options(headers: {'Accept': 'application/json'}),
+    );
+    return response.data['access_token'];
+  } catch (e) {
+    logger.e('🔁 accessToken 발급 실패: $e');
+    return null;
+  }
+}
