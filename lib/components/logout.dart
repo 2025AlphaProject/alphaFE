d7eed2ad6be93d194fb9eb506b06582fc7a6a3cb
47813 <vanguard0956@gmail.com>
2025-05-03 18:42:48 +0900

    [FEAT]: 로그아웃 기능 추가

---
+++ lib/components/logout.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+Future<void> Logout(BuildContext context) async {
+  final shouldLogout = await showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('로그아웃'),
+        content: const Text('정말 로그아웃하시겠습니까?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('취소'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('로그아웃'),
+          ),
+        ],
+      );
+    },
+  );
+
+  if (shouldLogout == true) {
+    const secureStorage = FlutterSecureStorage();
+
+    await secureStorage.delete(key: 'access_token');
+    await secureStorage.delete(key: 'refresh_token');
+
+    if (!context.mounted) {
+      return;
+    }
+
+    Phoenix.rebirth(context);
+  }
+}
+
+void buildApp({required Widget home}) {
+  runApp(
+    MaterialApp(
+      locale: const Locale('ko', 'KR'),
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+        Locale('en', 'US')
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: home,
+    ),
+  );
+}
