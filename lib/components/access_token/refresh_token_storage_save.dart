c0aaaeaa4dabe4c84050687d6de3dd2104e35a2f
47813 <vanguard0956@gmail.com>
2025-05-12 15:36:52 +0900

    [REFACTOR]: 불필요한 파일 삭제

--- lib/components/access_token/refresh_token_storage_save.dart
+++ lib/components/access_token/refresh_token_storage_save.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 const FlutterSecureStorage secureStorage = FlutterSecureStorage();
 
@@ -23,27 +22,5 @@ Future<String?> getRefreshToken() async {
 Future<void> deleteRefreshToken() async {
   if (!kIsWeb) {
     await secureStorage.delete(key: 'refresh_token');
-  }
-}
-
-// 엑세스 토큰 저장
-Future<void> saveAccessToken(String? token) async {
-  if (!kIsWeb) {
-    await secureStorage.write(key: 'access_token', value: token);
-  }
-}
-
-// 엑세스 토큰 읽기
-Future<String?> getAccessToken() async {
-  if (!kIsWeb) {
-    return await secureStorage.read(key: 'access_token');
-  }
-  return null;
-}
-
-// 엑세스 토큰 삭제
-Future<void> deleteAccessToken() async {
-  if (!kIsWeb) {
-    await secureStorage.delete(key: 'access_token');
   }
 }
