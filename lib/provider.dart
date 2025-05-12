9f0c493b1a3d1e4f0b1452ff5f137fed515df59d
47813 <vanguard0956@gmail.com>
2025-05-12 15:37:10 +0900

    [REFACTOR]: Provider 설정 변경

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,8 +1,8 @@
-import 'package:alpha_fe/pages/loading_page/loading_view.dart';
+import 'package:alpha_fe/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 
 List<SingleChildWidget> appProviders = [
-  ChangeNotifierProvider(create: (_) => LoadingViewModel()),
+  ChangeNotifierProvider(create: (_) => AuthProvider()),
 ];
