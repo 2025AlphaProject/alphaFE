ed3fbe259960381e67193fd9ef5d7443e26f8f84
47813 <vanguard0956@gmail.com>
2025-05-14 13:49:06 +0900

    [REFACTOR]: Provider 방식으로 API 요청 방식 변경

---
+++ lib/services/network/recommend_place/recommend_place.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/foundation.dart';
+import '../../dio/authorized_dio.dart';
+
+class RecommendPlaceService {
+  static const _baseUrl = 'http://conever.duckdns.org:8000';
+
+  /// 사용자 ID를 최대 3회까지 시도해서 받아옴
+  static Future<int?> fetchUserId(BuildContext context) async {
+    final dio = await getAuthorizedDio(context);
+    for (int i = 0; i < 3; i++) {
+      try {
+        final response = await dio.get('$_baseUrl/user/me/');
+        return response.data['sub'];
+      } catch (_) {
+        await Future.delayed(const Duration(seconds: 2));
+      }
+    }
+    return null;
+  }
+
+  /// 추천 장소용 웹소켓 채널 생성
+  static WebSocketChannel openRecommendationChannel({
+    required int userId,
+    required String district,
+    required int uniqueCode,
+  }) {
+    final url =
+        'ws://conever.duckdns.org:8000/tour/recommend/?user_id=$userId&areaCode=1&unique_code=$uniqueCode&days=1&sigunguName=$district';
+    return WebSocketChannel.connect(Uri.parse(url));
+  }
+
+  /// 랜덤 서울시 행정구역명 하나 리턴
+  static String getRandomDistrict() {
+    const districts = [
+      "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구",
+      "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구", "성북구",
+      "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"
+    ];
+    return districts[Random().nextInt(districts.length)];
+  }
+
+  /// 이미지 URL을 플랫폼에 따라 안전하게 변환
+  static String formatImageUrl(String? originalUrl) {
+    if (originalUrl == null || originalUrl.isEmpty) return '';
+    final secureUrl = originalUrl.replaceFirst('http://', '');
+    return kIsWeb
+        ? 'https://images.weserv.nl/?url=$secureUrl'
+        : 'http://$secureUrl';
+  }
+}
