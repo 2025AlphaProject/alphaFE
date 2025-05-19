e545ee3cf81c82dd1cf2ed639ffea45c1f25a69d
lee <iamlyg9667@kookmin.ac.kr>
2025-05-19 18:51:34 +0900

    [MODIFY]: homepage 위젯 분리, viewmodel 분리

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -23,7 +23,7 @@ class _MainScreenState extends State<MainScreen> {
   // 하단 탭에 연결될 페이지 목록
   List<Widget> get _pages => [
 
-    HomePage(accessToken: widget.accessToken,),
+    HomePage(),
     PlanPage(accessToken: widget.accessToken,),
     AddPage_0(accessToken: widget.accessToken,),
     kIsWeb ? MyPage_Web() : MyPage(accessToken: widget.accessToken,),
